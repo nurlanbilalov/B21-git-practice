cec4a7f43e89079a0452236f68aefa65a64b7c72
Nurlan <nurlanbilalov@gmail.com>
2021-01-26 13:43:47 -0500

    B21 learning Git

---
+++ src/main/java/Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("B22 future SDET learning Git");
+    }
+
+}
